ef900a07ca6eae9a58e2b3dad06a138ca3055cfb
Viniciusmm10 <93295720+Viniciusmm10@users.noreply.github.com>
2021-11-15 23:27:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+/* abre e fecha menu quando clicar no icon ''show'' */
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu fechar menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
